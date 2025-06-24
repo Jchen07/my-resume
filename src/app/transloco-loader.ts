2353be17677eae9e8052232c2b4cba6b6fc528a2
jchen <jchen070702@gmail.com>
2025-06-24 17:29:17 +0200

    feat: add build-info generation and integrate timestamp in translation loader

--- src/app/transloco-loader.ts
+++ src/app/transloco-loader.ts
@@ -2,13 +2,15 @@ import { inject, Injectable } from '@angular/core';
 import { Translation, TranslocoLoader } from '@jsverse/transloco';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '@/environments/environment';
+import { BUILD_TIMESTAMP } from '@/build-info';
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
   private http = inject(HttpClient);
 
-  // TODO: fer que cada desplegament recarregi tots els json
   getTranslation(lang: string) {
-    return this.http.get<Translation>(`${environment.baseUrl}/i18n/${lang}.json`);
+    return this.http.get<Translation>(
+      `${environment.baseUrl}/i18n/${lang}.json?v=${BUILD_TIMESTAMP}`
+    );
   }
 }
