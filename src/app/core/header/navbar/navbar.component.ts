ad00aaa0ffa1c5be7239710a8995b7d8bf8707d9
jchen <jchen070702@gmail.com>
2025-07-12 23:36:29 +0200

    feat: add FontAwesome icons and clipboard functionality for email in navbar

--- src/app/core/header/navbar/navbar.component.ts
+++ src/app/core/header/navbar/navbar.component.ts
@@ -2,20 +2,27 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TranslocoDirective } from '@jsverse/transloco';
 import { OpenLinkConfirmationDirective } from '@/app/core/shared/directives/open-link-confirmation.directive';
 import { environment } from '@/environments/environment';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'jc-navbar',
-  imports: [TranslocoDirective, OpenLinkConfirmationDirective],
+  imports: [TranslocoDirective, OpenLinkConfirmationDirective, FaIconComponent],
   templateUrl: './navbar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarComponent {
+  private static mail = 'jchen070702@gmail.com';
+
   options: { labelkey: string; href?: string; extern?: boolean }[] = [
     { labelkey: 'home', href: `${environment.baseHref}/home#home` },
     { labelkey: 'experience', href: `${environment.baseHref}/home#experience` },
     { labelkey: 'education', href: `${environment.baseHref}/home#education` },
     // { labelkey: 'projects' },
     // { labelkey: 'about-me' },
-    { labelkey: 'contact', href: 'mailto:jchen070702@gmail.com', extern: true }, // TODO: canviar per un pagina de contacte amb opció de copiar email
+    { labelkey: 'contact', href: `mailto:${NavbarComponent.mail}`, extern: true }, // TODO: canviar per un pagina de contacte amb opció de copiar email
   ];
+
+  copyEmailToClipboard() {
+    navigator.clipboard.writeText(NavbarComponent.mail);
+  }
 }
