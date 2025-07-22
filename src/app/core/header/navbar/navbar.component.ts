71b52c6080fe438d2f9c6212448a94ca8f54eb5d
jchen <jchen070702@gmail.com>
2025-07-22 20:10:01 +0200

    refactor: update navbar component to use GlobalConstants for email and improve class usage
    - Replaced hardcoded email with GlobalConstants.email for better maintainability.
    - Enhanced HTML structure in navbar.component.html for consistent cursor pointer styles and improved readability.

--- src/app/core/header/navbar/navbar.component.ts
+++ src/app/core/header/navbar/navbar.component.ts
@@ -3,6 +3,13 @@ import { TranslocoDirective } from '@jsverse/transloco';
 import { OpenLinkConfirmationDirective } from '@/app/core/shared/directives/open-link-confirmation.directive';
 import { environment } from '@/environments/environment';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { GlobalConstants } from '@/app/core/shared/constants/global.constants';
+
+interface NavbarOption {
+  labelkey: string;
+  href?: string;
+  extern?: boolean;
+}
 
 @Component({
   selector: 'jc-navbar',
@@ -11,18 +18,16 @@ import { FaIconComponent } from '@fortawesome/angular-fontawesome';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarComponent {
-  private static mail = 'jchen070702@gmail.com';
-
-  options: { labelkey: string; href?: string; extern?: boolean }[] = [
+  options: NavbarOption[] = [
     { labelkey: 'home', href: `${environment.baseHref}/home#home` },
     { labelkey: 'experience', href: `${environment.baseHref}/home#experience` },
     { labelkey: 'education', href: `${environment.baseHref}/home#education` },
     // { labelkey: 'projects' },
     // { labelkey: 'about-me' },
-    { labelkey: 'contact', href: `mailto:${NavbarComponent.mail}`, extern: true },
+    { labelkey: 'contact', href: `mailto:${GlobalConstants.email}`, extern: true },
   ];
 
   copyEmailToClipboard() {
-    navigator.clipboard.writeText(NavbarComponent.mail);
+    navigator.clipboard.writeText(GlobalConstants.email);
   }
 }
