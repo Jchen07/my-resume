ffcc3deac64665537a465d0539592366fbb82c16
jchen <jchen070702@gmail.com>
2025-07-23 21:59:39 +0200

    refactor: improve button styling and class binding in buttons bar
    - Replaced buttonStyles with basicButtonStyles and basicButtonColors for clearer class management.
    - Updated button class bindings in buttons-bar.component.html for consistency and readability.

--- src/app/pages/home/presentation-section/buttons-bar/buttons-bar.component.ts
+++ src/app/pages/home/presentation-section/buttons-bar/buttons-bar.component.ts
@@ -12,8 +12,10 @@ import { GlobalConstants } from '@/app/core/shared/constants/global.constants';
 })
 export class ButtonsBarComponent {
   // tailwind dark: doesn't work with @apply
-  protected buttonStyles =
-    'flex items-center text-sm sm:text-base gap-0.5 sm:gap-1.5 rounded-lg border border-gray-400 fill-neutral-800 px-1 sm:px-2.5 py-1 sm:py-0.5 hover:bg-neutral-900 hover:fill-neutral-200 hover:text-neutral-200 dark:fill-neutral-200 dark:hover:bg-gray-200 dark:hover:fill-neutral-800 dark:hover:text-neutral-700';
+  protected basicButtonStyles =
+    'flex items-center text-sm sm:text-base gap-0.5 sm:gap-1.5 rounded-lg  fill-neutral-800 px-1 sm:px-2.5 py-1 sm:py-0.5 cursor-pointer';
+  protected basicButtonColors =
+    'border border-gray-400 hover:bg-neutral-900 hover:fill-neutral-200 hover:text-neutral-200 dark:fill-neutral-200 dark:hover:bg-gray-200 dark:hover:fill-neutral-800 dark:hover:text-neutral-700';
 
   protected copyEmail() {
     navigator.clipboard.writeText(GlobalConstants.email);
