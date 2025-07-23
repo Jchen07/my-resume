420c8a4d80cda179a41ea44632c50b8849afc2e9
jchen <jchen070702@gmail.com>
2025-07-23 22:04:49 +0200

    refactor: update button styles in buttons bar for improved aesthetics
    - Modified basicButtonColors to enhance visual appeal and consistency with the overall design.
    - Adjusted border and background colors for better user interaction feedback.

--- src/app/pages/home/presentation-section/buttons-bar/buttons-bar.component.ts
+++ src/app/pages/home/presentation-section/buttons-bar/buttons-bar.component.ts
@@ -15,7 +15,7 @@ export class ButtonsBarComponent {
   protected basicButtonStyles =
     'flex items-center text-sm sm:text-base gap-0.5 sm:gap-1.5 rounded-lg  fill-neutral-800 px-1 sm:px-2.5 py-1 sm:py-0.5 cursor-pointer';
   protected basicButtonColors =
-    'border border-gray-400 hover:bg-neutral-900 hover:fill-neutral-200 hover:text-neutral-200 dark:fill-neutral-200 dark:hover:bg-gray-200 dark:hover:fill-neutral-800 dark:hover:text-neutral-700';
+    'border border-gray-300 bg-white text-gray-800 hover:bg-gray-100 hover:border-gray-400';
 
   protected copyEmail() {
     navigator.clipboard.writeText(GlobalConstants.email);
